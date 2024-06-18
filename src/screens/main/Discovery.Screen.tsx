ffd3075b3de6b214ddb5ee74caccabaf50d44ff3
ShahinHB <shahinhb@code.edu.az>
2024-06-18 17:11:55 +0400

    Create Discover Screen

--- src/screens/main/Discovery.Screen.tsx
+++ src/screens/main/Discovery.Screen.tsx
@@ -1,12 +1,30 @@
-import {View, Text} from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import React from 'react';
+import DiscoverCard from 'components/DiscoverCard';
+import { categories } from '../../mock/Discover.Mock';
 
 const DiscoveryScreen = () => {
   return (
-    <View>
-      <Text>DiscoveryScreen</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      {Object.keys(categories).map((key) => (
+        <DiscoverCard
+          key={key}
+          categoryName={categories[key]?.categoryName}
+          backgroundColor={categories[key]?.backgroundColor}
+          backgroundImage={categories[key]?.backgroundImage}
+        />
+      ))}
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    gap: 25,
+    paddingTop: 25,
+    alignItems: 'center',
+  },
+});
 
 export default DiscoveryScreen;
